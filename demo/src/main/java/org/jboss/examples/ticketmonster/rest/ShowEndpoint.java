b39e4b863518ec4fa3e0cf1399a62c487d611c49
pszuster <pablo.szuster@gmail.com>
2015-08-28 23:59:51 -0300

    asdf

--- demo/src/main/java/org/jboss/examples/ticketmonster/rest/ShowEndpoint.java
+++ demo/src/main/java/org/jboss/examples/ticketmonster/rest/ShowEndpoint.java
@@ -62,7 +62,6 @@ public class ShowEndpoint
    public Response findById(@PathParam("id") Long id)
    {
 
-       System.out.println("****************************************************************")
       TypedQuery<Show> findByIdQuery = em.createQuery("SELECT DISTINCT s FROM Show s LEFT JOIN FETCH s.event LEFT JOIN FETCH s.venue LEFT JOIN FETCH s.performances LEFT JOIN FETCH s.ticketPrices WHERE s.id = :entityId ORDER BY s.id", Show.class);
       findByIdQuery.setParameter("entityId", id);
       Show entity;
