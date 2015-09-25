bcd2a3f73c923ec32384b63ac924013df44e3a9d
ARGENTINA <rhforum.ops.ar@gmail.com>
2015-09-25 17:34:00 -0300

    Update TicketPrice.java
    prueba

--- demo/src/main/java/org/jboss/examples/ticketmonster/model/TicketPrice.java
+++ demo/src/main/java/org/jboss/examples/ticketmonster/model/TicketPrice.java
@@ -127,7 +127,7 @@ public class TicketPrice implements Serializable {
     }
 
     public float getPrice() {
-        return price;
+        return price;//
     }
 
     public void setPrice(float price) {
